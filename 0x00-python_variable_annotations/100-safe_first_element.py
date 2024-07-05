96f0d2d0bddeb3456eae224150e91583e8241f2c
therealmahmoud <moha91875460@gmail.com>
2024-07-05 03:27:06 +0300

    Updated

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ A type-annotated function. """
-from typing import Any, Sequence, Union, TypeVar
-
+from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
